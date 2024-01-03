c7f76649e29f7c81853cf83cf3be2d6514cd958f
Yash Suhagiya <54718565+yashsuhagiya@users.noreply.github.com>
2024-01-03 20:42:03 +0100

    add: secret key

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,12 +29,12 @@ environ.Env.read_env(env_file = os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*********'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
