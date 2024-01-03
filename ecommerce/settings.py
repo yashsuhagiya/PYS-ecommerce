6fb423d5640d26f463402385e8f3524016275884
Yash Suhagiya <54718565+yashsuhagiya@users.noreply.github.com>
2024-01-03 20:50:58 +0100

    fix: vercel file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -155,6 +155,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'shop/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/images/'
 
@@ -176,3 +177,8 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '**'
 
 SOCIAL_AUTH_INSTAGRAM_KEY = '**'        # App ID
 SOCIAL_AUTH_INSTAGRAM_SECRET = '**'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
