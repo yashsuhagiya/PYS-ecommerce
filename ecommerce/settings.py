d2636e873da0897083cf36a486189436fa9aeb8f
Yash Suhagiya <54718565+yashsuhagiya@users.noreply.github.com>
2024-01-08 17:57:02 +0100

    add: product to databse

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': '30845',
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
